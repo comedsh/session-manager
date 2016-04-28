bee3e10707615453c2509d598284501898026452
shangyang <comedshang@163.com>
2016-04-28 15:34:33 +0800

    stable, the serializable version of the author and refactor some method name for better understandable

--- tomcat-redis-session-manager/src/main/java/org/ranran/tomcat/redissessions/Serializer.java
+++ tomcat-redis-session-manager/src/main/java/org/ranran/tomcat/redissessions/Serializer.java
@@ -1,12 +1,44 @@
 package org.ranran.tomcat.redissessions;
 
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public interface Serializer {
+	
   void setClassLoader(ClassLoader loader);
+  
+  /**
+   * 
+   * Serialize the session attributes into bytes; <br> 
+   * 
+   * then, SessionSerializationMetadata will enwrap the bytes for serialization. <br>
+   * 
+   * 
+   * @param session
+   * @return
+   * @throws IOException
+   */
+  byte[] makeBindaryData(RedisSession session) throws IOException;
+  
+  /**
+   * 
+   * Serialize the @see SessionSerializationMetadata which enwraps the binary data from @see JavaSerializer#makeBindaryData(RedisSession)
+   * 
+   * @param session
+   * @param metadata
+   * @return
+   * @throws IOException
+   */
+  byte[] makeSerialData(RedisSession session, SessionSerializationMetadata metadata) throws IOException;
+  
+  /**
+   * deserialize into session
+   * 
+   * @param data
+   * @param session
+   * @param metadata
+   * @throws IOException
+   * @throws ClassNotFoundException
+   */
+  void deserialize(byte[] data, RedisSession session, SessionSerializationMetadata metadata) throws IOException, ClassNotFoundException;
 
-  byte[] attributesHashFrom(RedisSession session) throws IOException;
-  byte[] serializeFrom(RedisSession session, SessionSerializationMetadata metadata) throws IOException;
-  void deserializeInto(byte[] data, RedisSession session, SessionSerializationMetadata metadata) throws IOException, ClassNotFoundException;
 }
